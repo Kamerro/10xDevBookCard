b3fceb2ee33f6d01b58c8e3ae206dfd125d5245e
Kamil Mikuła <kamimik090@student.polsl.pl>
2026-02-01 19:42:28 +0100

    deploy config

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+import os
 
 from app.api.router import api_router
 from app.web.router import router as web_router
@@ -8,7 +9,9 @@ from app.web.router import router as web_router
 def create_app() -> FastAPI:
     app = FastAPI(title="BookCards")
 
-    app.mount("/static", StaticFiles(directory="static"), name="static")
+    # Use absolute path for static files in Docker
+    static_dir = os.path.join(os.getcwd(), "static")
+    app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
     app.include_router(api_router, prefix="/api")
     app.include_router(web_router)
